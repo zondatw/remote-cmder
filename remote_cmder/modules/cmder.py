23a83ce03198ff473ffc90f5982ec3f6279ad144
zonda yang <u226699@gmail.com>
2023-02-13 20:44:49 +0800

    Add new cmd: sha1

--- remote_cmder/modules/cmder.py
+++ remote_cmder/modules/cmder.py
@@ -5,6 +5,7 @@ class Cmder:
     def __init__(self):
         self.__cmd_map = {
             "md5": self.__cmd_md5,
+            "sha1": self.__cmd_sha1,
         }
 
     def execute(self, cmd, filename, data, *args, **kwargs):
@@ -15,6 +16,15 @@ class Cmder:
 
     def __cmd_md5(self, filename, data, *args, **kwargs):
         m = hashlib.md5()
+        m.update(data)
+        h = m.hexdigest()
+        return (
+            True,
+            f"{filename}: {h}",
+        )
+
+    def __cmd_sha1(self, filename, data, *args, **kwargs):
+        m = hashlib.sha1()
         m.update(data)
         h = m.hexdigest()
         return (
--- remote_cmder/test/test_cmder.py
+++ remote_cmder/test/test_cmder.py
@@ -8,6 +8,7 @@ class TestCmder:
     def test_cmder_supported(self):
         test_cases = [
             "md5",
+            "sha1",
         ]
         for test_case in test_cases:
             assert self.cmder.is_cmd_supported(test_case) is True
@@ -30,4 +31,17 @@ class TestCmder:
         assert exec_result == (
             True,
             "test: 202cb962ac59075b964b07152d234b70",
+        )
+
+    def test_cmd_sha1(self):
+        result = self.cmder._Cmder__cmd_sha1("test", "123".encode())
+        assert result == (
+            True,
+            "test: 40bd001563085fc35165329ea1ff5c5ecbdbbeef",
+        )
+
+        exec_result = self.cmder.execute("sha1", "test", "123".encode())
+        assert exec_result == (
+            True,
+            "test: 40bd001563085fc35165329ea1ff5c5ecbdbbeef",
         )
