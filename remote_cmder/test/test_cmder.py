f2badd0b878a96a7f7aa44da7962ce63cabc3956
zonda yang <u226699@gmail.com>
2023-02-23 23:42:36 +0800

    Modify test_cmder.py

--- remote_cmder/test/test_cmder.py
+++ remote_cmder/test/test_cmder.py
@@ -1,9 +1,11 @@
 from remote_cmder.modules.cmder import Cmder
+from remote_cmder.modules.cmd import default_cmd_map
 
 
 class TestCmder:
     def setup_class(self):
         self.cmder = Cmder()
+        self.cmder.registers(default_cmd_map)
 
     def test_cmder_supported(self):
         test_cases = [
@@ -21,12 +23,6 @@ class TestCmder:
             assert self.cmder.is_cmd_supported(test_case) is False
 
     def test_cmd_md5(self):
-        result = self.cmder._Cmder__cmd_md5("test", "123".encode())
-        assert result == (
-            True,
-            "test: 202cb962ac59075b964b07152d234b70",
-        )
-
         exec_result = self.cmder.execute("md5", "test", "123".encode())
         assert exec_result == (
             True,
@@ -34,12 +30,6 @@ class TestCmder:
         )
 
     def test_cmd_sha1(self):
-        result = self.cmder._Cmder__cmd_sha1("test", "123".encode())
-        assert result == (
-            True,
-            "test: 40bd001563085fc35165329ea1ff5c5ecbdbbeef",
-        )
-
         exec_result = self.cmder.execute("sha1", "test", "123".encode())
         assert exec_result == (
             True,
